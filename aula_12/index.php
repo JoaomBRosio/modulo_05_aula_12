8f0e7e75416502d75a058c277fa0118863f701c9
melon musk <125475444+JoaomBRosio@users.noreply.github.com>
2023-03-02 23:33:58 -0300

    Add files via upload

---
+++ aula_12/index.php
@@ -0,0 +1,45 @@
+<?php
+//Lacos de repetição
+
+
+//LACO DE REPETIÇÃO 'FOR'
+//comeca no '0' e vai até o '10' (tudo oque esta dentro das chaaves).
+/*for($contador=0;$contador<10;$contador++){
+    echo 'Ola mundo';
+    echo '<br>';
+    echo $contador;
+    echo '<hr>';
+}
+
+//comeca no '0' e vai até o '10' (tudo oque esta dentro das chaaves).
+for($contador=10;$contador>0;$contador--){
+    echo 'Ola mundo';
+    echo '<br>';
+    echo $contador;
+    echo '<hr>';
+}
+*/
+
+/*
+//LACO DE REPETIÇÃO 'WHILE'
+
+$contador = 0;
+while($contador < 10){
+    echo 'salveeeeeeeeeeeee';
+    echo '<hr>';
+    $contador+=1;
+}
+*/
+
+//LACO DE REPETICAO 'DO'
+
+$contador = 0;
+do{
+    echo 'ola pessoal';
+    echo '<br>';
+    $contador++;
+}while($contador < 10);
+
+
+
+?>
